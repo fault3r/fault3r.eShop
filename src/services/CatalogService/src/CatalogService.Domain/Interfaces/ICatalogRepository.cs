f0644769fd79368dafa8ed5111367f8350414754
Hamed Damavandi <hamed.damaavandi@gmail.com>
2025-09-14 23:08:46 +0330

    initial repository

--- src/services/CatalogService/src/CatalogService.Domain/Interfaces/ICatalogRepository.cs
+++ src/services/CatalogService/src/CatalogService.Domain/Interfaces/ICatalogRepository.cs
@@ -1,10 +1,19 @@
 
 using System;
+using CatalogService.Domain.Entities;
 
 namespace CatalogService.Domain.Interfaces
 {
     public interface ICatalogRepository
     {
+        Task<IEnumerable<Item>> GetAllAsync();
 
+        Task<(bool Success, Item? Item)> GetByIdAsync(string id);
+
+        Task<(bool Success, Item? Item)> CreateAsync(Item item);
+
+        Task<(bool Success, Item? Item)> UpdateAsync(Item item);
+
+        Task<bool> DeleteAsync(string id);
     }
 }
--- src/services/CatalogService/src/CatalogService.Infrastructure/Repositories/CatalogRepository.cs
+++ src/services/CatalogService/src/CatalogService.Infrastructure/Repositories/CatalogRepository.cs
@@ -1,13 +1,37 @@
 using System;
+using CatalogService.Domain.Entities;
 using CatalogService.Domain.Interfaces;
+using CatalogService.Infrastructure.Data.Contexts;
 
 namespace CatalogService.Infrastructure.Repositories
 {
-    public class CatalogRepository : ICatalogRepository
+    public class CatalogRepository(CatalogContext context) : ICatalogRepository
     {
-        public CatalogRepository()
+        private readonly CatalogContext context = context;
+
+        public Task<IEnumerable<Item>> GetAllAsync()
         {
-            
+            throw new NotImplementedException();
+        }
+
+        public Task<(bool Success, Item? Item)> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(bool Success, Item? Item)> CreateAsync(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(bool Success, Item? Item)> UpdateAsync(Item item)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public Task<bool> DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
