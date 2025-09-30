756b107206b4a19ea7a9ce94ced828ddb9d87a1d
Hamed Damavandi <hamed.damaavandi@gmail.com>
2025-09-30 21:42:49 +0330

    some changes

--- src/services/CatalogManagementService/src/CatalogManagementService.Infrastructure/obj/Debug/net8.0/CatalogManagementService.Infrastructure.AssemblyInfo.cs
+++ src/services/CatalogManagementService/src/CatalogManagementService.Infrastructure/obj/Debug/net8.0/CatalogManagementService.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogManagementService.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edff6b006aa9db2d6d0fe05da7384dad51a9ac81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e29edcfa895e023094fb05dbd1a1396445674542")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogManagementService.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogManagementService.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
