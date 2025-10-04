88dc15303feb0af98905344f174bfe1cbf7d9524
Hamed Damavandi <hamed.damaavandi@gmail.com>
2025-10-05 00:00:32 +0330

    some updates

--- src/services/CatalogManagementService/src/CatalogManagementService.Api/obj/Debug/net8.0/CatalogManagementService.Api.AssemblyInfo.cs
+++ src/services/CatalogManagementService/src/CatalogManagementService.Api/obj/Debug/net8.0/CatalogManagementService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogManagementService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc400e96dafc9a7ff6fd81faafc6a2dfd717aa4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfde571d7b6ed408b096bf8965dac18a5021eb9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogManagementService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogManagementService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
