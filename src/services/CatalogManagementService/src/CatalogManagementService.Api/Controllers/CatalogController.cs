9ec1480985e3bec49ab5f03be93f630e1c8ada05
Hamed Damavandi <hamed.damaavandi@gmail.com>
2025-10-05 18:33:15 +0330

    add controller returns using actionresult methods

--- src/services/CatalogManagementService/src/CatalogManagementService.Api/Controllers/CatalogController.cs
+++ src/services/CatalogManagementService/src/CatalogManagementService.Api/Controllers/CatalogController.cs
@@ -13,13 +13,17 @@ namespace CatalogManagementService.Api.Controllers
     [ApiVersion("1.0")]
     public class CatalogController(IMediator mediator) : ControllerBase
     {
-        private readonly IMediator _mediator = mediator;
+        private readonly IMediator _mediator = mediator;        
 
         [HttpGet]
         public async Task<ActionResult> GetAllAsync()
         {
             var (Code, Items) = await _mediator.Send(new GetAllQuery());
-            return StatusCode(Code, Items);
+            return Code switch
+            {
+                200 => Ok(Items),
+                _ => StatusCode(Code),
+            };
         }
 
         [HttpGet]
@@ -27,16 +31,24 @@ namespace CatalogManagementService.Api.Controllers
         public async Task<ActionResult> GetByIdAsync([FromRoute] string id)
         {
             var (Code, Item) = await _mediator.Send(new GetByIdQuery { Id = id });
-            if (Code == 404)
-                return NotFound();
-            return StatusCode(Code, Item);
+            return Code switch
+            {
+                200 => Ok(Item),
+                400 => BadRequest(),
+                404 => NotFound(),
+                _ => StatusCode(Code),
+            };
         }
 
         [HttpPost]
         public async Task<ActionResult> CreateAsync([FromBody] CreateItemDto item)
         {
             var (Code, Item) = await _mediator.Send(new CreateCommand { Item = item });
-            return StatusCode(Code, Item);
+            return Code switch
+            {
+                201 => CreatedAtAction(nameof(GetByIdAsync), new { id = Item.Id }, Item),
+                _ => StatusCode(Code),
+            };
         }
 
         [HttpPut]
--- src/services/CatalogManagementService/tests/CatalogManagementService.Tests/obj/Debug/net8.0/CatalogManagementService.Tests.AssemblyInfo.cs
+++ src/services/CatalogManagementService/tests/CatalogManagementService.Tests/obj/Debug/net8.0/CatalogManagementService.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogManagementService.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfde571d7b6ed408b096bf8965dac18a5021eb9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88dc15303feb0af98905344f174bfe1cbf7d9524")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogManagementService.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogManagementService.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
