0b9560c33ab2a2e26e2eb33503bad84f0bf93b13
Hamed Damavandi <hamed.damaavandi@gmail.com>
2025-12-07 23:49:14 +0330

    remove Log prefix in ILogger

--- src/services/UserService/src/UserService.Infrastructure/Logging/ILogger.cs
+++ src/services/UserService/src/UserService.Infrastructure/Logging/ILogger.cs
@@ -5,15 +5,15 @@ namespace UserService.Infrastructure.Logging;
 
 public interface ILogger
 {
-    void LogInformation(string log);
-    void LogWarning(string log);
-    void LogError(string log);
+    void Information(string log);
+    void Warning(string log);
+    void Error(string log);
 
-    void LogError(Exception exception, string log);
+    void Error(Exception exception, string log);
 
-    void LogInformation(string log, IDictionary<string, object> properties);
-     void LogWarning(string log, IDictionary<string, object> properties);
-    void LogError(string log, IDictionary<string, object> properties);
+    void Information(string log, IDictionary<string, object> properties);
+     void Warning(string log, IDictionary<string, object> properties);
+    void Error(string log, IDictionary<string, object> properties);
 
-    void LogError(Exception exception, string log, IDictionary<string, object> properties);
+    void Error(Exception exception, string log, IDictionary<string, object> properties);
 }
--- src/services/UserService/src/UserService.Infrastructure/Logging/SerilogLogger.cs
+++ src/services/UserService/src/UserService.Infrastructure/Logging/SerilogLogger.cs
@@ -1,43 +1,44 @@
+
 using System;
-using System.Collections.Generic;
-using Serilog;
 
 namespace UserService.Infrastructure.Logging;
 
-public sealed class SerilogLogger(Serilog.ILogger logger) : ILogger
+public sealed class Serilogger(Serilog.ILogger logger) : ILogger
 {
     private readonly Serilog.ILogger _logger = logger;
 
-    public void LogInformation(string log) => _logger.Information(log);
+    public void Information(string log) => _logger.Information(log);
 
-    public void LogWarning(string log) => _logger.Warning(log);
+    public void Warning(string log) => _logger.Warning(log);
 
-    public void LogError(string log) => _logger.Error(log);
+    public void Error(string log) => _logger.Error(log);
 
-    public void LogError(Exception exception, string log)
+    public void Error(Exception exception, string log)
         => _logger.Error(exception, log);
 
-    public void LogInformation(string log, IDictionary<string, object> properties)
-        => ContextualLogger(properties).Information(log);
+    public void Information(string log, IDictionary<string, object> properties)
+        => Contextualger(properties).Information(log);
 
-    public void LogWarning(string log, IDictionary<string, object> properties)
-        => ContextualLogger(properties).Warning(log);
+    public void Warning(string log, IDictionary<string, object> properties)
+        => Contextualger(properties).Warning(log);
 
-    public void LogError(string log, IDictionary<string, object> properties)
-        => ContextualLogger(properties).Error(log);
+    public void Error(string log, IDictionary<string, object> properties)
+        => Contextualger(properties).Error(log);
 
-    public void LogError(Exception exception, string log, IDictionary<string, object> properties)
-        => ContextualLogger(properties).Error(exception, log);
+    public void Error(Exception exception, string log, IDictionary<string, object> properties)
+        => Contextualger(properties).Error(exception, log);
 
-    private Serilog.ILogger ContextualLogger(IDictionary<string, object>? properties)
+    private Serilog.ILogger Contextualger(IDictionary<string, object>? properties)
     {
-        var contextualLogger = _logger;
-        if (properties != null)
-            foreach (var kvp in properties)
-            {
-                contextualLogger = contextualLogger
-                    .ForContext(kvp.Key, kvp.Value, destructureObjects: true);
-            }
-        return contextualLogger;
+        if (properties is null)
+            return _logger;
+
+        var contextualger = _logger;
+        foreach (var kvp in properties)
+        {
+            contextualger = contextualger
+                .ForContext(kvp.Key, kvp.Value, destructureObjects: true);
+        }
+        return contextualger;
     }
 }
