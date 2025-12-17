566e5e8d86db08e21723c4845f5f778c67c67e0a
Hamed Damavandi <hamed.damaavandi@gmail.com>
2025-12-18 01:17:46 +0330

    add readme

--- src/services/UserService/src/UserService.Api/Middlewares/CrossCuttingMiddleware.cs
+++ src/services/UserService/src/UserService.Api/Middlewares/CrossCuttingMiddleware.cs
@@ -8,30 +8,35 @@ namespace UserService.Api.Middlewares;
 
 public class CrossCuttingMiddleware(RequestDelegate next)
 {
-    private const string header = "X-Correlation-ID";
+    private const string headerName = "X-Correlation-ID";
 
     private readonly RequestDelegate _next = next;
 
     public async Task InvokeAsync(
-        HttpContext context, ICorrelationContext correlationContext)
+        HttpContext context,
+        ICorrelationContext correlationContext)
     {
-        var correlationId = context.Request.Headers[header]
+        if (correlationContext is null)
+            throw new ArgumentNullException(nameof(correlationContext));
+        
+        var correlationId = context.Request.Headers[headerName]
             .FirstOrDefault()
             ?? Guid.NewGuid().ToString();
 
         correlationContext.Set(correlationId);
-        context.Items[header] = correlationId;
-        context.Response.Headers[header] = correlationId;
+        
+        context.Items[headerName] = correlationId;
+        context.Response.Headers[headerName] = correlationId;
 
         using (LogContext.PushProperty("CorrelationId", correlationId))
         {            
             Log.Information(
-                "Incoming request {Method} {Path}", context.Request.Method, context.Request.Path);   
+                "Incoming request {Method} {Path}.", context.Request.Method, context.Request.Path);   
 
             await _next(context);
 
             Log.Information(
-                "Completed request {Method} {Path} with status {StatusCode}", context.Request.Method, context.Request.Path, context.Response.StatusCode);       
+                "Completed request {Method} {Path} with status {StatusCode}.", context.Request.Method, context.Request.Path, context.Response.StatusCode);       
         }
     }
 }
