0fe264d0fba942808697cfa054576ec689a766ad
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-01-12 17:29:15 +0330

    updates

--- src/services/UserService/src/UserService.Application/Messaging/Notifications/UserRegisteredNotification.cs
+++ src/services/UserService/src/UserService.Application/Messaging/Notifications/UserRegisteredNotification.cs
@@ -7,8 +7,8 @@ namespace UserService.Application.Messaging.Notifications;
 
 public sealed record UserRegisteredNotification : INotification
 {
-    public string Email { get; }
-    public string FullName { get; }
+    public string Email { get; init; }
+    public string FullName { get; init; }
 
     public UserRegisteredNotification(
         string email,
