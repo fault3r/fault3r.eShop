6e48dd7b5cb68db81086aae3ba515413a1331d8c
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-02-26 13:15:14 +0330

    updates

--- src/services/UserService/src/UserService.Infrastructure/DependencyInjection/MassTransitExtensions.cs
+++ src/services/UserService/src/UserService.Infrastructure/DependencyInjection/MassTransitExtensions.cs
@@ -1,8 +1,11 @@
 
 using System;
+using MassTransit;
+using MassTransit.EntityFrameworkCoreIntegration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using UserService.Infrastructure.Exceptions.DependencyInjection;
+using UserService.Infrastructure.Persistence.Contexts;
 using UserService.Infrastructure.Settings;
 
 namespace UserService.Infrastructure.DependencyInjection;
@@ -18,7 +21,26 @@ public static class MassTransitExtensions
             .Get<RabbitmqSettings>()
         ?? throw new MissingRabbitmqSettingsException();
 
-        
+        services.AddMassTransit(config =>
+        {
+            config.AddEntityFrameworkOutbox<EfPostgresDbContext>(cfg =>
+            {
+                cfg.QueryDelay = TimeSpan.FromSeconds(1);
+                cfg.UsePostgres();
+                cfg.UseBusOutbox(); 
+            });
+
+            config.UsingRabbitMq((ctx, cfg) =>
+            {
+                cfg.Host(settings.HostName, c =>
+                {
+                    c.Username(settings.UserName);
+                    c.Password(settings.Password);
+                });
+
+                // cfg.ReceiveEndpoint("user-events", e => { ... });
+            });
+        });
 
         return services;
     }
