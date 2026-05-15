ab0911e86c376da4d605292b3b77031c7726b042
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-05-15 10:17:12 +0330

    ..

--- src/services/UserService/tests/JIT/Program.cs
+++ src/services/UserService/tests/JIT/Program.cs
@@ -5,8 +5,24 @@ namespace JIT;
 
 internal class Program
 {
-    public static async Task Main(string[] args)
+    public static void Main(string[] args)
     {
-        
+
+    }
+}
+
+public class Downloader
+{
+    public static async Task<string> Download(
+        string url,
+        CancellationToken cancellationToken = default)
+    {
+        Console.WriteLine("start downloading..");
+
+        await Task.Delay(3000, cancellationToken);
+
+        Console.WriteLine("downloaded.");
+
+        return "data";
     }
 }
