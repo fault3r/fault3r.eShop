cddc224e20a24bbade2274dd0eb7a416773869fd
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-05-15 11:02:35 +0330

    ..

--- src/services/UserService/tests/JIT/Program.cs
+++ src/services/UserService/tests/JIT/Program.cs
@@ -1,28 +1,50 @@
 ﻿
 using System;
+using System.Threading.Tasks;
 
 namespace JIT;
 
 internal class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
+        Console.WriteLine("app started.");
 
+        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(2));
+
+        var downloader = new Downloader();
+
+        var task = downloader.Download("url", cts.Token);
+
+        var data = await task;
+
+        Console.WriteLine($"downloaded: {data}");
+
+        Console.WriteLine("everything is ended.");
     }
 }
 
 public class Downloader
 {
-    public static async Task<string> Download(
+    public async Task<string> Download(
         string url,
         CancellationToken cancellationToken = default)
     {
-        Console.WriteLine("start downloading..");
+        try
+        {
+            Console.WriteLine("downloading..");
 
-        await Task.Delay(3000, cancellationToken);
+            await Task.Delay(3000, cancellationToken);
 
-        Console.WriteLine("downloaded.");
+            Console.WriteLine("downloaded.");
 
-        return "data";
+            return "data";
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("canceled.");
+
+            return "nothing";
+        }
     }
 }
