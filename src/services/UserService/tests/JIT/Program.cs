a1635e126deb33f4cacdb835681ebc081688cf39
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-05-19 21:59:20 +0330

    ....

--- src/services/UserService/tests/JIT/Program.cs
+++ src/services/UserService/tests/JIT/Program.cs
@@ -10,41 +10,7 @@ internal class Program
     {
         Console.WriteLine("app started.");
 
-        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(2));
-
-        var downloader = new Downloader();
-
-        var task = downloader.Download("url", cts.Token);
-
-        var data = await task;
-
-        Console.WriteLine($"downloaded: {data}");
 
         Console.WriteLine("everything is ended.");
-    }
-}
-
-public class Downloader
-{
-    public async Task<string> Download(
-        string url,
-        CancellationToken cancellationToken = default)
-    {
-        try
-        {
-            Console.WriteLine("downloading..");
-
-            await Task.Delay(3000, cancellationToken);
-
-            Console.WriteLine("downloaded.");
-
-            return "data";
-        }
-        catch (OperationCanceledException)
-        {
-            Console.WriteLine("canceled.");
-
-            return "nothing";
-        }
     }
 }
