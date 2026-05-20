5ee7d302aaeeac4ab6ddc0a327292edeae025890
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-05-20 16:33:19 +0330

    ...

--- src/services/UserService/tests/UserService.Tests/MainTest.cs
+++ src/services/UserService/tests/UserService.Tests/MainTest.cs
@@ -1,35 +1,13 @@
 
 using System;
-using System.Text.Json;
-using UserService.Domain.Aggregates.UserAggregate;
-using UserService.Domain.ValueObjects;
-using UserService.Infrastructure.CrossCutting.JsonSerializer;
 
 namespace UserService.Tests;
 
 public class MainTests
 {
     [Fact]
-    public void Test()
+    public void TestName()
     {
-        var user = User.Create(
-            Identity.From(Guid.NewGuid()),
-            Email.Parse("test@example.com"),
-            PasswordHash.Parse("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"),
-            PasswordSalt.Parse("salt"),
-            FullName.From("John", "Doe"),
-            Role.User,
-            Status.Pending
-        );
-
-        var options = new AppJsonSerializer().DefaultOptions;
-
-        var @event = user.Events.FirstOrDefault();
-
-        var serialized = JsonSerializer.Serialize(@event, @event!.GetType(), options);
-
-        var obj = JsonSerializer.Deserialize(serialized,  @event.GetType(), options);
-
-        Assert.True(true);
+        
     }
 }
