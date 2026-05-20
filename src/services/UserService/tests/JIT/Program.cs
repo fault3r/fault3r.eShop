81d594dc8aed4bae8d3670bac17a32afcaebd690
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-05-20 17:05:23 +0330

    test...

--- src/services/UserService/tests/JIT/Program.cs
+++ src/services/UserService/tests/JIT/Program.cs
@@ -9,7 +9,30 @@ internal class Program
     {
         Console.WriteLine("app started.");
 
-
         Console.WriteLine("everything is ended.");
+    }
+}
+
+
+public class DownloadEventArgs(string filename) : EventArgs
+{
+    public string Filename { get; init; } = filename;
+}
+
+public sealed class Downloader
+{
+    public event EventHandler<DownloadEventArgs>? OnComplete;
+
+    public async Task<string> Download(string url)
+    {
+        Console.WriteLine($"downloading {url}...");
+
+        await Task.Delay(3000);
+
+        Console.WriteLine("downloaded.");
+
+        OnComplete?.Invoke(this,new DownloadEventArgs("test.file"));
+
+        return "data";
     }
 }
