8b988dcb9c7e9917c080c41d1ed70900222e7971
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-05-22 09:18:46 +0330

    new test

--- src/services/UserService/tests/JIT/Program.cs
+++ src/services/UserService/tests/JIT/Program.cs
@@ -9,7 +9,19 @@ internal class Program
     {
         Console.WriteLine("app started.");
 
-        
+
         Console.WriteLine("everything is ended.");
     }
 }
+
+public enum GenderType
+{
+    Male = 1,
+    Female = 0,
+}
+
+public sealed class Gender(GenderType type)
+{
+    public GenderType Value { get; init; } = type;
+}
+
