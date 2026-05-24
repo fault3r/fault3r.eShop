9aabc8ef8d463cced0c631891b21d718ea249bae
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-05-24 16:49:38 +0330

    reset tests

--- src/services/UserService/tests/JIT/Program.cs
+++ src/services/UserService/tests/JIT/Program.cs
@@ -9,34 +9,6 @@ internal class Program
     {
         Console.WriteLine("app started.");
 
-        var me = new Fault3r();
-        Console.WriteLine($"{me.Name}, {me.Gender.Value}");
-
         Console.WriteLine("everything is ended.");
     }
-}
-
-public enum GenderType
-{
-    Male = 1,
-    Female = 0,
-}
-
-public class Gender(GenderType type)
-{
-    public GenderType Value { get; init; } = type;
-
-    public static readonly Gender Male = new(GenderType.Male);
-}
-
-public class Human
-{
-    public Gender Gender { get; set; }
-     = Gender.Male;
-}
-
-public sealed class Fault3r : Human
-{
-    public string Name { get; set; }
-        = "Hamed Damavandi";
 }
