55ca60279d1c71e3bb8d425c8bd5df1a37ebeb1d
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-05-24 16:49:59 +0330

    ...

--- src/services/UserService/tests/JIT/Program.cs
+++ src/services/UserService/tests/JIT/Program.cs
@@ -7,8 +7,5 @@ internal class Program
 {
     public static async Task Main(string[] args)
     {
-        Console.WriteLine("app started.");
-
-        Console.WriteLine("everything is ended.");
     }
 }
