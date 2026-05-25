6d42e6d76fbbea45790ce1cefdc1a4b3e80929c9
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-05-25 20:00:28 +0330

    write a test arch

--- src/services/UserService/tests/JIT/Program.cs
+++ src/services/UserService/tests/JIT/Program.cs
@@ -2,3 +2,33 @@
 using System;
 
 namespace JIT;
+
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("app started.");
+        
+        Console.WriteLine("everything is ended.");
+    }
+}
+
+
+
+#region  Domain
+
+public interface IValueObject { }
+
+public abstract class ValueObject : IValueObject { }
+
+public class GenderValueObject(
+    string email
+) : ValueObject
+{
+    public string Email { get; init; } = email;
+}
+
+
+
+#endregion
