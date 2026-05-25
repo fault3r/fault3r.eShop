f0cdb09f92dbd060f3a4aa43b64caa2c0eec68d4
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-05-25 20:28:32 +0330

    domain

--- src/services/UserService/tests/JIT/Program.cs
+++ src/services/UserService/tests/JIT/Program.cs
@@ -8,9 +8,15 @@ class Program
 {
     public static async Task Main(string[] args)
     {
-        Console.WriteLine("app started.");
-        
-        Console.WriteLine("everything is ended.");
+        Console.WriteLine("\napp started.");
+
+        var user = User.Create("fault3r@mail.com");
+
+        Console.WriteLine($"Email: {user.Email}");
+        foreach (var item in user.Events)
+            Console.WriteLine($"{item.Id} : {item.Message}");
+
+        Console.WriteLine("\neverything is ended.");
     }
 }
 
@@ -22,13 +28,40 @@ public interface IValueObject { }
 
 public abstract class ValueObject : IValueObject { }
 
-public class GenderValueObject(
+public sealed class EmailValueObject(
     string email
 ) : ValueObject
 {
     public string Email { get; init; } = email;
+
+    public override string ToString() => Email;
 }
 
+public record DomainEvent(Guid Id, string Message);
 
+public class Entity
+{
+    public Guid Id { get; init; }
+}
+
+public abstract class AggregateRoot : Entity
+{
+    public List<DomainEvent> Events = [];
+}
+
+public class User(
+    EmailValueObject email
+) : AggregateRoot
+{
+    public EmailValueObject Email { get; init; } = email;
+
+    public static User Create(string email)
+    {
+        var user = new User(new EmailValueObject(email));
+        user.Events.Add(new DomainEvent(Guid.NewGuid(), "User Created!"));
+        return user;
+    }
+
+}
 
 #endregion
