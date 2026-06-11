1d437d2379b5ffa366803f5f8489554786f04848
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-06-11 14:39:43 +0330

    clean JIT test

--- src/services/UserService/tests/JIT/Program.cs
+++ src/services/UserService/tests/JIT/Program.cs
@@ -3,91 +3,12 @@ using System;
 
 namespace JIT;
 
-
 class Program
 {
     public static async Task Main(string[] args)
     {
         Console.WriteLine("app started.\n");
 
-        var user = UserFactory.CreateUser("test@mail.com");
-
-        user.ChangeEmail("fault3r@mail.com");
-
-        Console.WriteLine($"Email: {user.Email}");
-
-        foreach (var item in user.Events)
-            Console.WriteLine(item.Message);
-
         Console.WriteLine("\neverything is ended.");
     }
 }
-
-#region  Domain
-
-public interface IValueObject { }
-
-public abstract class ValueObject : IValueObject { }
-
-public sealed class EmailValueObject(
-    string email
-) : ValueObject
-{
-    public string Value { get; set; } = email;
-
-    public override string ToString() => Value;
-}
-
-public sealed record DomainEvent
-{
-    public string Message { get; init; }
-
-    public DomainEvent(string message)
-        => Message = $"[{DateTime.Now}] " + message;
-}
-
-public class Entity(
-    Guid id
-)
-{
-    public Guid Id { get; init; } = id;
-}
-
-public interface IAggregateRoot { }
-
-public abstract class AggregateRoot(Guid id) : Entity(id), IAggregateRoot
-{
-    private readonly List<DomainEvent> events = [];
-
-    public IReadOnlyCollection<DomainEvent> Events => [.. events];
-
-    public void RaiseEvent(DomainEvent @event) => events.Add(@event);
-
-    protected void ClearEvents() => events.Clear();
-}
-
-public sealed class User(
-    Guid id,
-    EmailValueObject email
-) : AggregateRoot(id)
-{
-    public EmailValueObject Email { get; private set; } = email;
-
-    public void ChangeEmail(string email)
-    {
-        Email.Value = email;
-        RaiseEvent(new DomainEvent($"{Id} email changed to {email}."));
-    }
-}
-
-public static class UserFactory
-{
-    public static User CreateUser(string email)
-    {
-        var user = new User(Guid.NewGuid(), new EmailValueObject(email));
-        user.RaiseEvent(new DomainEvent($"{user.Id} created with email {user.Email}."));
-        return user;
-    }
-}
-
-#endregion
