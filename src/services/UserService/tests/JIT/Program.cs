382b11dbed542df5045a57be92d441b490ae7619
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-06-16 17:37:27 +0330

    ...

--- src/services/UserService/tests/JIT/Program.cs
+++ src/services/UserService/tests/JIT/Program.cs
@@ -8,34 +8,35 @@ class Program
 {
     public static async Task Main(string[] args)
     {
-        Console.WriteLine("app started.\n");
-
+        string text = args[0] is null ? "" : args[0];
         var writer = new ConsoleWriter();
-
-        await writer.WriteAsync("fault3r",0,0);
-
-        Console.WriteLine("\neverything is ended.");
+        await writer.WriteAsync(text);
     }
 }
 
 public sealed class ConsoleWriter
 {
     private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private readonly Random random = new();
 
-    public async Task WriteAsync(string quote, int xPos, int yPos)
+    public async Task WriteAsync(string text)
     {
-        int d = 100;
-
-        byte[] buffer = new byte[d];
-        RandomNumberGenerator.Fill(buffer);
-
-        foreach(byte b in buffer)
+        foreach (char c in text)
         {
-            char c = Chars[b % Chars.Length];
+            (int left, int top) = Console.GetCursorPosition();
+
+            for (int i = 0; i < 5; i++)
+            {
+                int x = random.Next(0, 255);
+                char d = Chars[x % Chars.Length];
+
+                Console.Write(d);
+                Console.SetCursorPosition(left, top);
+
+                await Task.Delay(random.Next(0,80));
+            }
 
             Console.Write(c);
-
-            await Task.Delay(b);
         }
     }
 }
