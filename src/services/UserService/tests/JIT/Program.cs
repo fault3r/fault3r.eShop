bf9f480cc32468f3ba4b0382210462dd072e3f0b
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-06-17 22:27:59 +0330

    ...

--- src/services/UserService/tests/JIT/Program.cs
+++ src/services/UserService/tests/JIT/Program.cs
@@ -7,7 +7,7 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        Console.Write("enter your text: ");
+        Console.Write("ConsoleWriter: Enter your text: ");
 
         string text = Console.ReadLine() ?? "";
         
