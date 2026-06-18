5fb1f7226985ebc9a0f380d0bbe09d6157d34e5c
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-06-18 18:39:35 +0330

    .....

--- src/services/UserService/tests/JIT/Program.cs
+++ src/services/UserService/tests/JIT/Program.cs
@@ -7,42 +7,5 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        Console.Write("ConsoleWriter: ");
-
-        string text = Console.ReadLine() ?? "";
-        
-        var writer = new ConsoleWriter();
-        await writer.WriteAsync(text);
-    }
-}
-
-public sealed class ConsoleWriter
-{
-    private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    private readonly Random random = new();
-
-    public async Task WriteAsync(string text)
-    {
-        Console.WriteLine();
-
-        foreach (char c in text)
-        {
-            (int left, int top) = Console.GetCursorPosition();
-
-            for (int i = 0; i < 5; i++)
-            {
-                int x = random.Next(0, 255);
-                char d = Chars[x % Chars.Length];
-
-                Console.Write(d);
-                Console.SetCursorPosition(left, top);
-
-                await Task.Delay(random.Next(0, 50));
-            }
-
-            Console.Write(c);
-        }
-        
-        Console.WriteLine();
     }
 }
